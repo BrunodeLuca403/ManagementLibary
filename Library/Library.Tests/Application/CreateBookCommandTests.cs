c553f5b8cc85fe54921319ce44033e0054e62287
Bruno <brunodeluca@gmail.com>
2025-03-30 14:17:00 -0300

    Criação dos testes

---
+++ Library/Library.Tests/Application/CreateBookCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using FluentAssertions;
+using Library.Application.Commands.Book;
+using Library.Core.Entities;
+using Library.Core.Repository;
+using Library.Infrastructure.Repository;
+using Library.Tests.Fakes;
+using Moq;
+
+
+namespace Library.Tests.Application
+{
+    public class CreateBookCommandTests
+    {
+        [Fact]
+        public async Task Create_Book_Command_Handler_Are_Ok()
+        {
+
+            //var guid = Guid.NewGuid();
+
+            var repository = Mock.Of<IBookRepository>(r => r.CreateBookAsync(It.IsAny<Book>()) == Task.CompletedTask);
+            var command = FakeDataHelper.CreateFakerInsertBookCommand();
+
+            var handler = new CreateBookCommandHandler(repository);
+
+            var result = await handler.Handle(command, new CancellationToken());   
+
+
+            result.IsSuccess.Should().BeTrue();
+            result.Data.Should().Be(result.Data);
+
+            //Assert.Equals(guid, result.Data);
+        }
+    }
+}
---
+++ Library/Library.Tests/Fakes/FakeDataHelper.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Library.Application.Commands.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests.Fakes
+{
+    public class FakeDataHelper
+    {
+        private static readonly Faker _faker = new();
+
+        private static readonly Faker<CreateBookCommand> _createBookCommand = new Faker<CreateBookCommand>()
+          .RuleFor(c => c.Title, f => f.Company.CompanyName())
+          .RuleFor(c => c.Author, f => f.Person.FirstName)
+          .RuleFor(c => c.ISBN, f => f.Person.Cpf())
+          .RuleFor(C => C.Yearpublication, f => f.Date.Month());
+
+        public static CreateBookCommand CreateFakerInsertBookCommand() => _createBookCommand.Generate();
+
+
+
+    }
+}
